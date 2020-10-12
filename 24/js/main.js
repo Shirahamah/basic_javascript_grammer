ff02dc7ab640131df25af5c22549f7748c727d96
Shirahamah <shirahama.hiroki@runneres.co.jp>
2020-10-12 14:34:05 +0900

    #9 アロー関数部分のコメント解除

--- 24/js/main.js
+++ 24/js/main.js
@@ -4,10 +4,10 @@
 //     return a + b + c;
 // };
 
-// const sum = (a, b, c) => a + b + c;
+const sum = (a, b, c) => a + b + c;
 
-// const total = sum(1, 2, 3) + sum(3, 4, 5);
-// console.log(total);
+const total = sum(1, 2, 3) + sum(3, 4, 5);
+console.log(total);
 
 // const double = function(a) {
 //     return a * 2;
